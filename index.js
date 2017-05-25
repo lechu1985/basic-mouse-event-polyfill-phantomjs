83a012322b7ee8b1996939a3320184e8362a15ce
Leslaw Wasowicz <KGD734@motorolasolutions.com>
2017-05-25 19:20:16 +0200

    added support for special-keys in event

--- index.js
+++ index.js
@@ -9,12 +9,19 @@
   // Polyfills DOM4 MouseEvent
 
   var MouseEvent = function (eventType, params) {
-    params = params || { bubbles: false, cancelable: false };
+    params = params || {
+        bubbles: false,
+        cancelable: false,
+        ctrlKey: false,
+        altKey: false,
+        shiftKey: false,
+        metaKey: false
+      };
     var mouseEvent = document.createEvent('MouseEvent');
-    mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, 0, 0, 0, 0, params.ctrlKey, params.altKey, params.shiftKey, params.metaKey, 0, null);
 
     return mouseEvent;
-  }
+  };
 
   MouseEvent.prototype = Event.prototype;
 
